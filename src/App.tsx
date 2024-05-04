ab2cc8e0bc7ad1b5ad8652829cd5c85203396a27
Munkhuunuu0603 <naranmunkhuu@gmail.com>
2024-05-04 17:57:32 +0800

    product detail

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomeLayout from "./layout/HomeLayout";
 import Home from "./components/Home";
+import ProductDetail from "./components/ProductDetail";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeLayout />} >
               <Route index element={<Home />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
             </Route>
           </Routes>
         </Suspense>
--- src/shared/ProductItem.tsx
+++ src/shared/ProductItem.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 interface Props {
     data: any
@@ -6,18 +8,17 @@ interface Props {
 
 const ProductItem = ({ data }: Props) => {
     return (
-        <div className='relative rounded-md h-[300px] hover:border hover:border-black transition-all cursor-pointer'>
+        <Link to={`/product/${data?.id}`} className='relative rounded-md h-[300px] hover:border hover:border-black transition-all cursor-pointer'>
             <img className='w-full rounded-md h-full' src={data?.thumbnail} />
 
             <div className='absolute bottom-0 text-white'>
                 {data.title}
-                {data.thumbnail}
                 {data.category}
                 {data.ageRestriction}
                 {data.description}
             </div>
 
-        </div>
+        </Link>
     )
 }
 
