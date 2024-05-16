59138fb47e2d34ff956a2facc6a72ebdf14bbb31
sumbe <sumbe0119@gmail.com>
2024-05-16 22:24:43 +0800

    shad ui package add

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react'
 import Banner from '../shared/Banner'
 import ProductList from '../shared/ProductList'
 import { datas } from '../shared/dummy'
+import { Button } from '../ui/Button'
 
 const Home = () => {
     const [data, setData] = useState([])
+    const [category, setCategory] = useState([])
+    console.log("🚀 ~ Home ~ category:", category)
+
     const fetchData = () => {
         fetch('https://dummyjson.com/products')
             .then(res => res.json())
@@ -16,13 +20,34 @@ const Home = () => {
 
     useEffect(() => {
         fetchData();
+        fetchCategory()
     }, []);
 
 
+    const fetchCategory = () => {
+        fetch('http://localhost:8080/api/category/list')
+            .then(res => res.json())
+            .then(json => {
+
+                setCategory(json.categories
+                )
+
+            })
+            .catch(e => {
+                console.error('Error:', e);
+            });
+    };
+
     return (
         <div>
+            {category.map((e: any, index: number) => {
+                return (
+                    <div key={index}>{e.name}</div>
+                )
+            })}
             <Banner />
             <div className='container flex-col flex'>
+                <Button variant='default'>shad ui test title</Button>
                 <ProductList title='Онцлох манга' data={data} />
             </div>
         </div>
